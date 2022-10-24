cdf38593b6e4d3f165686de775a27c29fc6cec71
Junior PC <49306278+jvniorrr@users.noreply.github.com>
2022-10-23 23:31:20 -0700

    CP3

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,83 +1,85 @@
 package Main;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import Data.Vector2D;
+import Data.spriteInfo;
 import logic.Control;
 import timer.stopWatchX;
 
-public class Main{
+public class Main {
 	// Fields (Static) below...
 	public static Color c = new Color(0, 148, 156);
 	public static boolean isImageDrawn = false;
 	public static stopWatchX timer = new stopWatchX(150); // set flashing timer to 250 or 1/4 of second
-	// object to set coordinates of sprites
-	public static Vector2D bottomRightVec1 = new Vector2D(1105, 575); // bottom right corner coords. for sprite
 
 	// Queue to hold animation movements
 	public static Queue<Vector2D> vecs1 = new LinkedList<Vector2D>();
 	public static Queue<Vector2D> vecs2 = new LinkedList<Vector2D>();
-	
+
+	// collection to hold our animation frames
+	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
+	public static int currentSpriteIndex = 0;
+
 	// obj to hold current vec in use
 	public static Vector2D currentVec = new Vector2D(-100, -100); // position to start off screen intially.
 	// End Static fields...
-	
+
 	public static void main(String[] args) {
-		Control ctrl = new Control();				// Do NOT remove!
-		ctrl.gameLoop();							// Do NOT remove!
+		Control ctrl = new Control(); // Do NOT remove!
+		ctrl.gameLoop(); // Do NOT remove!
 	}
-	
+
 	/* This is your access to things BEFORE the game loop starts */
-	public static void start(){
-		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		if (vecs1.isEmpty()) {
-			for (int i=-128; i<1115+128;i+=16) {
-				vecs1.add(new Vector2D(i, 0));
+	public static void start() {
+		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite
+		// or drawString)
+		int counterRef = 0;
+		for (int i = -128; i < 1115 + 128; i += 16) {
+			if (counterRef == 0) {
+				sprites.add(new spriteInfo(new Vector2D(i, 0), "walk0"));
+				counterRef++;
+			} else if (counterRef == 1) {
+				sprites.add(new spriteInfo(new Vector2D(i, 0), "walk1"));
+				counterRef++;
+			} else if (counterRef == 2) {
+				sprites.add(new spriteInfo(new Vector2D(i, 0), "walk2"));
+				counterRef++;
+			} else {
+				sprites.add(new spriteInfo(new Vector2D(i, 0), "walk3"));
+				counterRef = 0;
 			}
 		}
-		// isImageDrawn = true;
 	}
-	
-	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
+
+	/*
+	 * This is your access to the "game loop" (It is a "callback" method from the
+	 * Control class (do NOT modify that class!))
+	 */
 	public static void update(Control ctrl) {
-		// TODO: This is where you can code! (Starting code below is just to show you how it works)		
-		ctrl.addSpriteToFrontBuffer(currentVec.getX(),currentVec.getY(), "fighter");		// Add a tester sprite to render list by tag (Remove later! Test only!)
+		// TODO: This is where you can code! (Starting code below is just to show you
+		// how it works)
+
+		spriteInfo currSprite = sprites.get(currentSpriteIndex);
+		ctrl.addSpriteToFrontBuffer(currSprite.getCoords().getX(), currSprite.getCoords().getY(), currSprite.getTag()); // Add a tester sprite to render list by tag (Remove later! Test only!)
 
 		if (timer.isTimeUp()) {
-				// update current vec
-				currentVec = vecs1.remove();
 
-				// add the removed vector to our vecs 2 so game can reset from left to right
-				vecs2.add(currentVec);
-				
-				if (vecs1.isEmpty()) {
-					while (!vecs2.isEmpty()) {
-						vecs1.add(vecs2.remove());
-					}
-				}
+			if (currentSpriteIndex == (sprites.size() - 1) ) {
+				currentSpriteIndex = 0;
+			} else {
+				currentSpriteIndex++;
+			}
 
-				// reset timer obj for next pass
-				timer.resetWatch();
+			// reset timer obj for next pass
+			timer.resetWatch();
 		}
-		ctrl.drawString(100, 500, "" + vecs1.size(), Color.red);
-		ctrl.drawString(100, 550, "" + vecs2.size(), Color.red);
 
-
-
-
-		/** DEFAULT: 1280 X 720 */
-		// ctrl.addSpriteToFrontBuffer(1115, 575, "f0");			// Add a tester sprite to render list by tag (Remove later! Test only!)
-		// ctrl.drawString(1105, 719, "Fernando Mendoza", c);		// Test drawing text on screen where you want (Remove later! Test only!)
-		
-		/** SCREEN TWEAKED FOR MAC:  1280 X 800 */
-		/** CHANGE RESOLUTION IN GRAPHICS CLASS; LINE 105~ TO 800 HEIGHT / Y PARAM. */
-		/** CHECKPOINT #1 */
-		// ctrl.addSpriteToFrontBuffer(1115, 660, "fighter");		// Add a tester sprite to render list by tag (Remove later! Test only!)
-		// ctrl.drawString(1105, 799, "Fernando Mendoza", c);		// Test drawing text on screen where you want (Remove later! Test only!)
 	}
-	
+
 	// Additional Static methods below...(if needed)
 
 }
